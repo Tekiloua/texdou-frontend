a821150a6c745acd52fbdf327613feb2351f7166
Tekiloua <rasoanaivopietro@gmail.com>
2026-05-02 12:22:03 +0200

    creation et design de base du navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,35 @@
+import { Link } from "react-router-dom"
+import { BookAIcon, Bot, File } from "lucide-react"
+import { useLocation } from "react-router-dom"
+
 export default function Navbar() {
+  const location = useLocation()
   return (
-    <nav className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto">
-        <h1 className="text-xl font-bold">My App</h1>
+    <div className="fixed z-20 flex h-[8vh] w-full items-center justify-between border-b bg-slate-50 px-10">
+      <div className="flex cursor-pointer items-center gap-1">
+        <BookAIcon className="size-5" />
+        <span className="text-lg font-bold">TEXDOU</span>
       </div>
-    </nav>
+      <div className="liens flex items-center gap-6 text-sm">
+        <Link
+          to={"/chatbot"}
+          className={`flex items-center gap-1 ${location.pathname == "/chatbot" ? "font-bold" : "font-normal text-gray-400"}`}
+        >
+          <Bot
+            className={`size-6 ${location.pathname == "/chatbot" ? "font-bold" : "font-normal text-gray-400"}`}
+          />{" "}
+          <span className="mt-1">Chatbot</span>
+        </Link>
+        <Link
+          to={"/documents"}
+          className={`flex items-center gap-1 ${location.pathname == "/documents" ? "font-bold" : "font-normal text-gray-400"}`}
+        >
+          <File
+            className={`size-5 ${location.pathname == "/documents" ? "font-bold" : "font-normal text-gray-400"}`}
+          />{" "}
+          <span className="mt-1">Documents</span>
+        </Link>
+      </div>
+    </div>
   )
 }
